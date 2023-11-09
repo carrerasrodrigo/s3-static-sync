0558d6ffb6992cf48ab28158a834db0a7a495a3c
Rodrigo N. Carreras <carrerasrodrigo@gmail.com>
2023-11-09 10:55:06 -0300

    dump new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='s3-static-sync',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     description='',
